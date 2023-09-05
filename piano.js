7383a02050b72075b3be53b594d71c963c110ef5
MSarari <new11111st@gmail.com>
2023-09-06 01:26:52 +0300

    The final project

---
+++ piano.js
@@ -0,0 +1,13 @@
+﻿const keys = document.querySelectorAll(".key");
+
+function playSound(note) {
+    const audio = new Audio('sounds'+ '/' +note+'.ogg');
+    audio.play();
+}
+
+keys.forEach(key => {
+    key.addEventListener("click", () => {
+        const note = key.id;
+        playSound(note);
+    });
+});
